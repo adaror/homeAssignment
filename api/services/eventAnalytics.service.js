08589d4b05c459be899b3004ab50408adee0e4c3
Or Adar <adaror@gmail.com>
2019-03-27 20:26:59 +0200

    seperate

--- api/services/eventAnalytics.service.js
+++ api/services/eventAnalytics.service.js
@@ -1,56 +1,5 @@
-const geoip = require('geoip-country');
-const uuidv1 = require('uuid/v1');
-const moment = require('moment');
 const PageViews = require('../models/PageViews');
-const PageViewsByBrowser = require('../models/PageViewsByBrowser');
 const db = require('../../config/db');
-
-function _validateEvent(event) {
-  return {
-    pageId: event.pageId ? event.pageId : '',
-    userId: event.userId ? event.userId : '',
-    timeStamp: event.timeStamp ? event.timeStamp : moment().format("YYYY-MM-DD HH:mm:ss"),
-  }
-}
-
-function _getCountryByIp(userIp) {
-  if (userIp) {
-    const geo = geoip.lookup(userIp);
-    return geo.country;
-  }
-  return 'no-country';
-}
-
-function _generateGuid() {
-  return uuidv1();
-}
-
-function _insertEventToPageView(id, event, userCountry) {
-    return PageViews.create({
-      id,
-      pageId: event.pageId,
-      userId: event.userId,
-      timeStamp: event.timeStamp,
-      country: userCountry
-    }).catch((err) => err);
-}
-
-function _insertEventToPageViewByBrowser(id, event, browserName) {
-  return PageViewsByBrowser.create({
-    pageViewId: id,
-    timeStamp: event.timeStamp,
-    browserName
-  }).catch((err) => err);
-}
-
-async function insertEvent (event, userIp, browserName) {
-  event = _validateEvent(event);
-  const userCountry = _getCountryByIp(userIp);
-  const pageViewGuid = _generateGuid();
-  await _insertEventToPageView(pageViewGuid, event, userCountry)
-  await _insertEventToPageViewByBrowser(pageViewGuid, event, browserName);
-  return;
-}
 
 function _getPageViewByPageId(pageId) {
   return PageViews.findAll({
